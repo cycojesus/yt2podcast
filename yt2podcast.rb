62d78cb149d3eaa1a6e6a15b960cc098ee2ff93e
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2016-04-05 13:52:18 +0200

    optional feed generation; audio files noved up from /medias/
    Signed-off-by: Gwenhael Le Moine <gwenhael.le.moine@gmail.com>

--- yt2podcast.rb
+++ yt2podcast.rb
@@ -11,60 +11,60 @@ require 'youtube-dl.rb'
 require 'taglib'
 
 CONFIG = { root_url: 'http://yt2podcast.le-moine.org',
-           download_root_path: './feeds' }
+           download_root_path: './feeds',
+           generate_feed: false }
 
 def youtubedl( url, output_file, downloaded_file )
-  begin
-    YoutubeDL.get( url,
-                   output: output_file,
-                   extract_audio: true,
-                   audio_format: 'vorbis',
-                   audio_quality: 0 ) unless File.exist? downloaded_file
-  rescue
-    STDERR.puts "Couldn't retrieve video #{url}"
-  end
+  YoutubeDL.get( url,
+                 output: output_file,
+                 extract_audio: true,
+                 audio_format: 'vorbis',
+                 audio_quality: 0 ) unless File.exist? downloaded_file
+rescue
+  STDERR.puts "Couldn't retrieve video #{url}"
 end
 
 def tag( entry, filename )
   TagLib::FileRef.open( filename ) do |file|
     tag = file.tag
-    
+
     tag.artist = entry.author.name.content
     tag.title = entry.title.content
-    
+
     file.save
   end
 end
 
 def convert_feed( url, config )
   feed = RSS::Parser.parse( open( url ), false )
-  
+
   name = feed.author.name.content
   STDOUT.puts name
-  
+
   feed_dir = "#{config[ :download_root_path ]}/#{name.tr( ' ', '_' )}"
-  dl_dir = "#{feed_dir}/medias"
+  dl_dir = "#{feed_dir}"
   FileUtils.mkdir_p( dl_dir ) unless Dir.exist?( dl_dir )
-  
+
   feed.entries
-      .each do |entry| 
+      .each do |entry|
     video_id = entry.link.href.gsub( 'http://www.youtube.com/watch?v=', '' )
     output_file = "#{dl_dir}/#{video_id}.tmp"
     downloaded_file = "#{dl_dir}/#{video_id}.ogg"
     STDOUT.puts "  🠖 #{video_id}"
-    
+
     youtubedl( entry.link.href, output_file, downloaded_file )
-    
+
     if File.exist?( downloaded_file )
       tag( entry, downloaded_file )
 
-      entry.link.href = "#{config[ :root_url ]}/#{name}/medias/#{video_id}.ogg"
+      entry.link.href = "#{config[ :root_url ]}/#{name}/#{video_id}.ogg" if CONFIG[ :generate_feed ]
     end
   end
-  
+
+  return unless CONFIG[ :generate_feed ]
   feed.updated = Time.now
-  
-  File.open( "#{feed_dir}/feed.xml", 'w' ) do |feed_file| 
+
+  File.open( "#{feed_dir}/feed.xml", 'w' ) do |feed_file|
     feed_file.write( feed.to_atom( 'feed' ).to_s )
   end
 end
@@ -73,7 +73,7 @@ def parse_opml( opml_node, parent_names=[] )
   feeds = []
   opml_node.elements.each('outline') do |el|
     feeds << parse_opml( el, parent_names + [ el.attributes['text'] ] ) if el.elements.size != 0
-    
+
     feeds << el.attributes['xmlUrl'] if el.attributes['xmlUrl']
   end
 
@@ -83,10 +83,6 @@ end
 feeds = []
 ARGV.options do |opts|
   #/ Usage: <progname> [options]...
-  opts.on( '-u', '--url=val', String ) { |val| CONFIG[ :root_url ] = val }
-  #/     -u <url> | --url=<url> : root url used in generated RSS
-  opts.on( '-o', '--output-dir=val', String ) { |val| CONFIG[ :download_root_path ] = val }
-  #/     -o <path> | --output-dir=<path> : directory where files are stored
   opts.on( '-i', '--input=val', String ) { |val| feeds << val }
   #/     -i <url> | --input=<url> : url of youtube feed
   opts.on( '-g', '--opml=val', String ) { |val|
@@ -94,7 +90,13 @@ ARGV.options do |opts|
     feeds.flatten
   }
   #/     -g <filepath> | --opml=<filepath> : path of opml file
-  
+  opts.on( '-o', '--output-dir=val', String ) { |val| CONFIG[ :download_root_path ] = val }
+  #/     -o <path> | --output-dir=<path> : directory where files are stored
+  opts.on( '-f', '--feed' ) { CONFIG[ :generate_feed ] = true }
+  #/     -g | --feed : enable generation of RSS feed (default: false)
+  opts.on( '-u', '--url=val', String ) { |val| CONFIG[ :root_url ] = val }
+  #/     -u <url> | --url=<url> : root url used in generated RSS
+
   opts.on_tail( '-h', '--help' ) { exec "grep '^  #/' < '#{__FILE__}'|cut -c6-" }
   #/     -h | --help : this
   opts.parse!
